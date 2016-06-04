d0e0532e3484271491daa98e34e5e34b3a36eed9
Eric Richardson <eric@ericonrails.com>
2016-06-03 17:03:39 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Team Selector
+
+A sinatra app for picking teams.
+
+You can make teams based on weather you want a certain number of team or certain size of team.
+
+The list of names you enter must be separated by commas.
---
+++ app.rb
@@ -0,0 +1,48 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+enable :sessions
+
+get '/' do
+  @teams = session[:teams]
+  erb :index, layout: :layout
+end
+
+post '/' do
+  names = params[:names].split(',')
+  teamsize = params[:teamsize].to_i
+  method = params[:method]
+  @teams = teams_by_size(names, teamsize) if method == "by_size"
+  @teams = teams_by_number(names, teamsize) if method == "by_teams"
+  @teams ||= "No method selected"
+  session[:teams] = @teams
+  erb :index, layout: :layout
+end
+
+def teams_by_size(names, teamsize, extras = 0)
+  return "Not enough people" if names.size < teamsize
+  teams = ''
+  team_number = 1
+    while names.size > 0 do
+      teams << "<div class=\"team\"><h2>Team #{team_number}</h2>"
+      teamsize.times do
+        if names.size > 0
+          teams << names.delete_at(rand(names.size)) + "<br>"
+        end
+      end
+      if extras > 0
+        teams << names.delete_at(rand(names.size)) + "<br>"
+        extras -=1
+      end
+      teams << "</div>"
+      team_number += 1
+    end
+  teams
+end
+
+def teams_by_number(names, number_of_teams)
+  return "You have to choose a number of teams ;("
+  teamsize = names.size / number_of_teams
+  extras = names.size % number_of_teams
+  return teams_by_size(names, teamsize, extras)
+end
